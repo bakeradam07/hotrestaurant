12110dae31a0cd64f884c7e6f87d29bcb28b2eb1
Ciprien Bossut <chefchino@live.com>
2019-11-26 19:41:30 -0500

    back end stuff need html to test

---
+++ server.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+
+var reservation = [
+    {
+        customerEmail: "chefchino",
+        customerId: "chino",
+        customerName: "Cip",
+        phoneNumber: "919-555-9876"
+    }
+];
+
+var waitingList = [
+    {
+        customerEmail: "jarez",
+        customerId: "456789",
+        customerName: "Jarez Howell",
+        phoneNumber: "919-555-9976"
+    }
+]
+// Connects to homepage
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+// connects to reservation page
+app.get("/reservation", function(req, res) {
+    res.sendFile(path.join(__dirname, "make.html"));
+});
+// Connects to tables.html
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+// Displays reservations
+app.get("/api/reservation", function(req, res) {
+    return res.json(reservation);
+});
+// Displays waiting list
+app.get("/api/waitingList", function(req, res) {
+    return res.json(waitingList);
+});
+
+// Display tables with first 5 of waiting list moved to reservations
+
+app.get("/api/waitingList/:reser", function(req, res) {
+    var tables = req.params.reser;
+
+    for ( var i = 0; i < waitingList.length; i++) {
+        if (reservations < 5) {
+            waitingList[0].push(reservation);
+            return res.json(reservation);
+        }
+    }
+    return res.json(false);
+});
+
+app.post("/api/waitingList", function(req, res) {
+    var newReservation = req.body;
+
+    newReservation.customerName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newReservation);
+    waitingList.push(newReservation);
+    res.json(newReservation);
+});
+
+app.listen(PORT, function() {
+    console.log("App Listening on PORT " + PORT);
+});
